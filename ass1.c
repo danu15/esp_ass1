4d5b02e207a2d93399ffda067c1403b54c3b3fd6
Boidl <32957199+Boidl@users.noreply.github.com>
2017-10-20 13:37:57 +0200

    C file added - Main

---
+++ ass1.c
@@ -0,0 +1,22 @@
+//-----------------------------------------------------------------------------
+// <Filename>.c
+//
+// <Explanation of the program ...>
+// <... May have multiple lines.>
+//
+// Group: Group <Group Number>, study assistant <Name of Study assistant>
+//
+// Authors: <Name> <Matriculum Number>
+// <Name> <Matriculum Number>
+// <Add one row for every additional group member>
+//-----------------------------------------------------------------------------
+//
+
+#include <stdio.h>
+
+int main( int argc, const char* argv[] )
+{
+	printf( "\nHello World\n\n" );
+	
+	return 0;
+}
